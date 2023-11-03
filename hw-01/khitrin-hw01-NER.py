3a9887109c0b18b1abba9272d541fd616d99055f
khit-mle <149804920+khit-mle@users.noreply.github.com>
2023-11-03 20:45:57 +0500

    NER model khitrin-hw01-NER.py

--- hw-01/khitrin-hw01-NER.py
+++ hw-01/khitrin-hw01-NER.py
@@ -1,1 +1,14 @@
 # ДЗ №1 по "Программное инженерии"
+# https://huggingface.co/dslim/bert-base-NER
+
+from transformers import AutoTokenizer, AutoModelForTokenClassification
+from transformers import pipeline
+
+tokenizer = AutoTokenizer.from_pretrained("dslim/bert-base-NER")
+model = AutoModelForTokenClassification.from_pretrained("dslim/bert-base-NER")
+
+nlp = pipeline("ner", model=model, tokenizer=tokenizer)
+example = "Microsoft opens a new campus in Florida in partnership with Google"
+
+ner_results = nlp(example)
+print(ner_results)
