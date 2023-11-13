5f168f1dc0d61b7264123adc64904f6fea45eba6
khit-mle <149804920+khit-mle@users.noreply.github.com>
2023-11-13 13:10:45 +0500

    Add print result as comment khitrin-hw01-NER.py

--- hw-01/khitrin-hw01-NER.py
+++ hw-01/khitrin-hw01-NER.py
@@ -12,3 +12,7 @@ example = "Microsoft opens a new campus in Florida in partnership with Google"
 
 ner_results = nlp(example)
 print(ner_results)
+
+# [{'entity': 'B-ORG', 'score': 0.99847335, 'index': 1, 'word': 'Microsoft', 'start': 0, 'end': 9}, \
+# {'entity': 'B-LOC', 'score': 0.9996697, 'index': 7, 'word': 'Florida', 'start': 32, 'end': 39}, \
+# {'entity': 'B-ORG', 'score': 0.99826354, 'index': 11, 'word': 'Google', 'start': 60, 'end': 66}]
